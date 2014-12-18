8bf24d63580cdfd30aaf28291805a10986317593
Patryk A. Wertka <spartanpageagain@gmail.com>
2014-12-18 10:58:52 +0100

    Im not going to prepare normal commit messages. Quiet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,56 @@
 #include <boost/asio/ssl.hpp>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
+#include <boost/signals2.hpp>
 
 #include <vector>
-#include <queue>
+#include <unordered_set>
 
-typedef boost::asio::ip::tcp::socket TcpSocket;
-typedef boost::asio::ssl::stream<TcpSocket> SslSocket;
-typedef boost::asio::ip::tcp::acceptor Acceptor;
-typedef boost::asio::ip::tcp::endpoint EndPoint;
+using TcpSocket = boost::asio::ip::tcp::socket;
+using SslSocket = boost::asio::ssl::stream<TcpSocket>;
+using Acceptor  = boost::asio::ip::tcp::acceptor;
+using EndPoint  = boost::asio::ip::tcp::endpoint;
+
+using Signals2Connection = boost::signals2::connection;
 
 class Session:
 	public std::enable_shared_from_this<Session>
 {
 public:
+	using Pointer     = std::shared_ptr<Session>;
+	using EndedSignal = boost::signals2::signal<void(Pointer, boost::system::error_code)>;
+
 	Session(TcpSocket);
 	virtual ~Session();
 
+	auto addEndedSignal(const EndedSignal::slot_type &) const -> Signals2Connection;
+
 	virtual auto start() -> void;
-	virtual auto end() -> void;
+	virtual auto end(boost::system::error_code) -> void;
 
 	auto socket() -> const TcpSocket &;
-protected:
-	TcpSocket _socket;
-	std::vector<char> _buffer;
 private:
 	virtual auto read() -> void;
 	virtual auto write() -> void;
+protected:
+	TcpSocket _socket;
+	std::vector<char> _buffer;
+	mutable EndedSignal _endedSignal;
+	boost::system::error_code _errorCode;
 };
 
 Session::Session(TcpSocket tcpSocket)
 	:_socket(std::move(tcpSocket)){}
 Session::~Session(){}
+auto Session::addEndedSignal(const EndedSignal::slot_type &slot) const -> Signals2Connection{
+	return _endedSignal.connect(slot);
+}
 auto Session::start() -> void{
 	read();
 }
-auto Session::end() -> void{}
+auto Session::end(boost::system::error_code ec) -> void{
+	_endedSignal(shared_from_this(), ec);
+}
 auto Session::socket() -> const TcpSocket &{
 	return _socket;
 }
@@ -44,7 +59,7 @@ auto Session::read() -> void{
 	boost::asio::async_read(
 		_socket, boost::asio::buffer(_buffer),
 		[this, self](boost::system::error_code ec, std::size_t){
-			if(!ec) write();
+			ec? end(ec) : write();
 		}
 	);
 }
@@ -53,21 +68,21 @@ auto Session::write() -> void{
 	boost::asio::async_write(
 		_socket, boost::asio::buffer(_buffer),
 		[this, self](boost::system::error_code ec, std::size_t){
-			if (!ec) read();
+			ec ? end(ec) : read();
 		}
 	);
 }
 
 class Server{
-	typedef std::shared_ptr<Session> SessionPointer;
-	typedef std::queue<SessionPointer> SessionContainer;
+	using SessionContainer = std::unordered_set<Session::Pointer>;
 public:
 	Server(boost::asio::io_service &, const EndPoint &);
 	virtual ~Server();
 	auto start() -> void;
 private:
 	virtual auto startAcceptingSessions() -> void;
-	virtual auto handleNewSession(SessionPointer) -> void;
+	virtual auto handleNewSession(Session::Pointer) -> void;
+	virtual auto onSessionEnd(Session::Pointer, boost::system::error_code) -> void;
 protected:
 	Acceptor _acceptor;
 	TcpSocket _socket;
@@ -88,16 +103,24 @@ auto Server::startAcceptingSessions() -> void{
 	_acceptor.async_accept(
 		_socket,
 		[this](boost::system::error_code ec){
-			if (!ec)
-				handleNewSession(std::make_shared<Session>(std::move(_socket)));
+			if(!ec) handleNewSession(std::make_shared<Session>(std::move(_socket)));
 			startAcceptingSessions();
 		}
 	);
 }
-auto Server::handleNewSession(SessionPointer sp) -> void{
-	_sessions.push(sp);
-	_sessions.back()->start();
+
+#pragma warning(push)
+#pragma warning(disable:4996)
+auto Server::handleNewSession(Session::Pointer sp) -> void{
+	_sessions.insert(sp);
+	sp->addEndedSignal(boost::bind(&Server::onSessionEnd, this, _1, _2));
+	sp->start();
 }
+#pragma warning(pop)
+auto Server::onSessionEnd(Session::Pointer sessionPtr, boost::system::error_code ec) -> void{
+	_sessions.erase(sessionPtr);
+}
+
 
 
 int main(){
