67c47edd7eb3d74c26956356fae38e04d1b3614a
Patryk A. Wertka <spartanpageagain@gmail.com>
2015-01-29 11:18:42 +0100

    Added new typedef 'cstrref'

--- include/typedefs.hpp
+++ include/typedefs.hpp
@@ -3,10 +3,10 @@
 #include <boost/asio/ssl.hpp>
 #include <boost/asio.hpp>
 #include <boost/signals2.hpp>
-
+#include <string>
 using TcpSocket = boost::asio::ip::tcp::socket;
 using SslSocket = boost::asio::ssl::stream<TcpSocket>;
 using Acceptor = boost::asio::ip::tcp::acceptor;
 using EndPoint = boost::asio::ip::tcp::endpoint;
-
+using cstrref = const std::string &;
 using Signals2Connection = boost::signals2::connection;
