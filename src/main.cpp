f76dc14bf4052090de92db59b87ec83cf6675ec8
Patryk A. Wertka <spartanpageagain@gmail.com>
2015-02-23 16:56:23 +0100

    ...Meh, new main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <boost/thread.hpp>
 
+#include "Server.hpp"
+#include "LoggingSession.hpp"
 #include "LoggingServer.hpp"
 
 #include <vector>
 
 //c10k - 10k clients!
 int main(){
+	using Server = LoggingServer<Server<std::vector<LoggingSession<>::Pointer>, void>>;
+
 	auto ioService = boost::asio::io_service();
 	auto port = 1337;
 	auto endPoint = EndPoint(boost::asio::ip::tcp::v4(), port);
-	auto server = LoggingServer<std::vector<LoggingSession::Pointer>, void>(ioService, endPoint);
+
+	auto server = Server(ioService, endPoint);
 	server.start();
 
 	while (true){}
