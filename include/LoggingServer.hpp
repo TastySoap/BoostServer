4a1ac5a0ac08d922760fea5745f9a04c771dfb50
Patryk A. Wertka <spartanpageagain@gmail.com>
2015-02-23 16:56:13 +0100

    Decorator pattern!

--- include/LoggingServer.hpp
+++ include/LoggingServer.hpp
@@ -3,25 +3,34 @@
 #include "Server.hpp"
 #include "LoggingSession.hpp"
 
-template<typename SessionContainer, typename Database>
-class LoggingServer : public Server<SessionContainer, Database>{
+template<typename TServer>
+class LoggingServer{
 public:
-	using SessionPtr = typename SessionContainer::value_type;
+	using Server = TServer;
+	using SessionPtr = typename Server::SessionContainer::value_type;
 
-	explicit LoggingServer(boost::asio::io_service &ioService, const EndPoint &endPoint): 
-		Server<SessionContainer, Database>(ioService, endPoint)
-	{ std::clog << __FUNCTION__ << std::endl; }
+	LoggingServer(boost::asio::io_service &ioService, const EndPoint &endPoint) :
+		_server(ioService, endPoint)
+	{ 
+		std::clog << __FUNCTION__ << std::endl; 
+	}
 
 	~LoggingServer()
 	{ std::clog << __FUNCTION__ << std::endl; }
 
-	auto handleNewSession(SessionPtr sessionPtr) -> void override{
-		std::clog << __FUNCTION__ << std::endl;
-		Server::handleNewSession(sessionPtr);
+	auto start() -> void{
+		_server.start();
 	}
 
-	auto onSessionEnd(SessionPtr sessionPtr, boost::system::error_code errorCode) -> void override{
+	auto handleNewSession(SessionPtr sessionPtr) -> void{
 		std::clog << __FUNCTION__ << std::endl;
-		Server::onSessionEnd(sessionPtr, errorCode);
+		_server.handleNewSession(sessionPtr);
 	}
+
+	auto onSessionEnd(SessionPtr sessionPtr, boost::system::error_code errorCode) -> void{
+		std::clog << __FUNCTION__ << std::endl;
+		server.onSessionEnd(sessionPtr, errorCode);
+	}
+private:
+	Server _server;
 };
--- include/LoggingSession.hpp
+++ include/LoggingSession.hpp
@@ -2,10 +2,41 @@
 
 #include "Session.hpp"
 
-class LoggingSession : public Session, public std::enable_shared_from_this<LoggingSession>{
-public:
-	explicit LoggingSession(TcpSocket &&);
-	virtual ~LoggingSession() override;
+#include <iostream>
 
-	auto read() -> void override;
+template<
+	/* Must offer constructor(TcpSocket &&) */
+	/* Must contain typename TcpSocket */
+	typename TSession = Session<>
+>
+class LoggingSession final: 
+	public std::enable_shared_from_this<LoggingSession<TSession>>
+{
+public:
+	using Session = TSession;
+	using TcpSocket = typename Session::TcpSocket;
+	using Pointer = std::shared_ptr<LoggingSession<Session>>;
+
+	LoggingSession(TcpSocket &&socket):
+		_session(std::move(socket))
+	{
+		std::clog << __FUNCTION__ << std::endl;
+	}
+
+	auto start() -> void{
+		std::clog << __FUNCTION__ << std::endl;
+		read();
+	}
+
+	auto read() -> void{
+		std::clog << __FUNCTION__ << std::endl;
+		_session.read();
+	};
+
+	auto end() -> void{
+		std::clog << __FUNCTION__ << std::endl;
+		_session.end();
+	}
+private:
+	Session _session;
 };
